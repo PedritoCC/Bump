ec9e9a1be95f7e8b93a54360a769183c18423686
PedritoCC <113424771+PedritoCC@users.noreply.github.com>
2023-02-08 11:39:26 +0100

    feat: algo ressort

--- logi.c
+++ logi.c
@@ -1,3 +1,5 @@
+#include <math.h>
+
 double force;
 double G;
 int w=3;
@@ -5,10 +7,14 @@ double param_ni;
 int hauteur;
 
 
-
 double diam_ressort(force, w, G){
-    return("d :" +sqrt((8*force*2*w^3)/(G*(0,3*w-1))));
+    return(sqrt((8*force*2*w^3)/(G*(0,3*w-1))));
 }
 
 double D; //diamètre réelle du ressort
-double longueur_vide(D,diam_ressort(force, w, G));
+double d;
+int n; //nombre de révolution
+int nt; // nombre total de spire 
+double longueur_vide(D, d, n){
+    return(n*(0.3*D+0.15*d)+ param_ni*d);
+}
